1a71ec366f93287ee0bb7d6652edd061b40eb028
Mola <18842653036@163.com>
2019-08-08 14:45:14 +0800

    practice

---
+++ 实验16：Scrapy 爬取实验楼课程信息/shiyanlou_courses_spider.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+import scrapy
+
+class ShiyanlouCoursesSpider(scrapy.Spider):
+
+	name = 'shiyanlou-courses'
+
+	@property
+	def start_urls(self):
+		
+		url_list = ['https://www.shiyanlou.com/courses/',
+					'https://www.shiyanlou.com/courses/?page=2',
+					'https://www.shiyanlou.com/courses/?page=3']
+		
+		return url_list		
+		
+	def parse(self,response):
+		
+		for course in response.css('div[class="col-sm-12 col-md-3"]'):
+			yield {
+				'name':course.css('h6.course-name::text').extract_first().strip(),
+				'description':course.css('div.course-description::text').extract_first().strip(),
+				'type':course.css('span.course-type::text').extract_first().strip(),
+				'students':course.css('span.data-v-610fe964::text').extract_first()
+			}
+
